631bc277b82b6181a1445af4e92f135d813f95a3
Aman Kothari <amankothari@Amans-MacBook-Air.local>
2026-03-28 23:57:28 +0530

    minor fixes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from app.api.routes import loan, application
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(loan.router, prefix="/api")
 app.include_router(application.router, prefix="/applications")
--- backend/app/models/application.py
+++ backend/app/models/application.py
@@ -1,10 +1,50 @@
 import uuid
-from backend.app.controllers.application_controller import validate_application
 
-applications_db = {}
+applications_db = {
+    "1": {
+        "id": "1",
+        "name": "Aman Kothari",
+        "income": 50000,
+        "employment": "Salaried",
+        "loan_amount": 500000,
+        "status": "ELIGIBLE",
+        "kyc": {"status": "VERIFIED", "score": 90},
+        "credit": {"status": "APPROVED", "score": 720, "active_loans": 1},
+    },
+    "2": {
+        "id": "2",
+        "name": "Rahul Sharma",
+        "income": 30000,
+        "employment": "Self-Employed",
+        "loan_amount": 800000,
+        "status": "NOT_ELIGIBLE",
+        "kyc": {"status": "VERIFIED", "score": 85},
+        "credit": {"status": "REJECTED", "score": 600, "active_loans": 6},
+    },
+    "3": {
+        "id": "3",
+        "name": "Priya Verma",
+        "income": 70000,
+        "employment": "Salaried",
+        "loan_amount": 300000,
+        "status": "KYC_COMPLETED",
+        "kyc": {"status": "VERIFIED", "score": 88},
+        "credit": None,
+    },
+    "4": {
+        "id": "4",
+        "name": "Rohit Gupta",
+        "income": 40000,
+        "employment": "Salaried",
+        "loan_amount": 200000,
+        "status": "DRAFT",
+        "kyc": None,
+        "credit": None,
+    },
+}
 
 def create_application(data):
-    validate_application(data)  
+    # validate_application(data)  
     app_id = str(uuid.uuid4())
 
     application = {
@@ -24,6 +64,7 @@ def create_application(data):
 
 def get_application(app_id):
     return applications_db.get(app_id)
+
 
 def get_all_applications():
     return list(applications_db.values())
